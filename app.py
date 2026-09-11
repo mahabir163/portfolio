81bd6cc698486d9901dc3c4d1502ba593bed9978
Mahabir <mahabirp2003@gmail.com>
2026-09-11 14:57:43 +0530

    Fix Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -29,4 +29,5 @@ def contact():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
